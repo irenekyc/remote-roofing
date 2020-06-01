0f109bc160ed962f225ff9c0fb9e73a61efb4170
irenekyc <gugu_irene@hotmail.com>
2020-06-01 12:08:31 +0800

    Updates

---
+++ precache-manifest.01bfeffdca500e3851f34bb939ff0d3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80104a7421a9feb28b71ccd87745d850",
+    "url": "/remote-roofing/index.html"
+  },
+  {
+    "revision": "05b7ecb6330d290a1996",
+    "url": "/remote-roofing/static/css/main.7d793458.chunk.css"
+  },
+  {
+    "revision": "1e92a4290c7660a14641",
+    "url": "/remote-roofing/static/js/2.1275d76c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/remote-roofing/static/js/2.1275d76c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05b7ecb6330d290a1996",
+    "url": "/remote-roofing/static/js/main.74d0e5f7.chunk.js"
+  },
+  {
+    "revision": "b39c3d9622d86fd3b4ec",
+    "url": "/remote-roofing/static/js/runtime-main.6eb5daf5.js"
+  }
+]);
